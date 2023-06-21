99705be9f21d9d88bc15a9b2541d73a21eac9a54
yctaii <yctai98@gmail.com>
2023-06-21 21:06:21 +0800

    add_employee

---
+++ src/main/Java/com/yc/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.yc.reggie.common;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 全局异常处理
+ */
+
+ @ControllerAdvice(annotations = {RestController.class, Controller.class})
+ @ResponseBody
+ @Slf4j
+public class GlobalExceptionHandler {
+    
+
+    /**
+     * 异常处理方法
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] res = ex.getMessage().split(" ");
+            String msg = res[2] + "已存在！";
+            return R.error(msg);
+        }
+        return R.error("出现异常");
+    }
+}
--- src/main/Java/com/yc/reggie/controller/EmployeeController.java
+++ src/main/Java/com/yc/reggie/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.yc.reggie.controller;
 
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,12 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    /**
+     * 
+     * @param request
+     * @param employee
+     * @return
+     */
     @PostMapping("/login")
     public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee) {
         //1、将收到的密码进行与数据库密码进行相同的加密
@@ -56,10 +64,41 @@ public class EmployeeController {
         return R.success(employe);
     }
 
+    /**
+     * 
+     * @param request
+     * @return
+     */
     @PostMapping("/logout")
     public R<String> logout(HttpServletRequest request) {
         //1、清理Session中保存的当前登陆员工的id
         request.removeAttribute("employee");
         return R.success("退出成功");
     }
+
+    /**
+     * 新增员工
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> addUser(HttpServletRequest request,@RequestBody Employee employee) {
+        log.info("收到员工信息：{}",employee.toString());
+
+        //在网页添加用户时，没有密码选项，给个默认赋值123456
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //添加用户时，还有个操作人信息,登录时存储的employee是存的他的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId) ;
+
+        //继承Mybatisplus中已有的保存方法，插入数据库
+        employeeService.save(employee);
+        return null;
+    }
+
 }
